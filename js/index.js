1ebc89ea19367c462cfacf8ea3930fbe12690bd4
Aleksandr <Alex2014wx@gmail.com>
2018-07-05 22:08:49 +0300

    Адаптация меню

--- js/index.js
+++ js/index.js
@@ -5,6 +5,42 @@ $( () => {
 		var windowTop = $(window).scrollTop();
 		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
 		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+		if(windowTop > 100)
+		{ 
+			$('#logo').css('width','200px');
+			$('#logo').css('height','90px');			   
+		} else 
+		{
+			$('#logo').css('width','250px');
+			$('#logo').css('height','115px');	
+		}
+		if($('nav').hasClass('navShadow')) 
+		{
+			$('#word-mark').css({'width':'0px','opacity':'0'});
+		} else
+		{
+			if($(window).width()>491)
+			{
+				$('#word-mark').css({'width':'120px','opacity':'1'});
+			}
+		}
+	});
+	
+	$(window).resize(()=>{
+		var width = $(window).width();
+		if(width <=491){ 
+			/*$('nav').addClass('navShadow');*/
+			$('#word-mark').css({'width':'0px','opacity':'0'});
+		} else {
+			/*$('nav').removeClass('navShadow');*/
+			if(!$('nav').hasClass('navShadow')) 
+			{
+				$('#word-mark').css({'width':'120px','opacity':'1'});
+			} else
+			{
+				$('#word-mark').css({'width':'0px','opacity':'0'});
+			}
+		}
 	});
 	
 	//Click Logo To Scroll To Top
